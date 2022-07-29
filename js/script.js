6b9c7470e13339804c18ebf6e8884cf36f3b91c8
Igor Tyan <tyanigor89@gmail.com>
2022-07-29 13:34:32 +0300

    init commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// name form
+let userName = prompt("Please enter your name");
+alert("Your name is " + userName);
+
+// math block
+let a = 9;
+let b = 3;
+
+alert(a + " + " + b + " = " + (a + b));
+alert(a + " - " + b + " = " + (a - b));
+alert(a + " * " + b + " = " + (a * b));
+alert(a + " / " + b + " = " + (a / b));
+alert(a + " ** " + b + " = " + (a ** b));
